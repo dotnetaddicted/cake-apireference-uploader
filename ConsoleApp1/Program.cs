6618cfc5a277eeba1158006b9dd3b76783b1d39a
Eugene Nikitenko <eugene.nikitenko@gmail.com>
2020-04-16 11:09:32 +0300

    Intermediate commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using Cake.ApiReference.Uploader;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Cake.ApiReference.Uploader.ApiRefUploaderAliases.UploadHtmlApiReferences(null,
+            new RestApiCredentials
+            {
+                UserName = "user",
+                Password = "pass",
+                Uri = "http://contoso.com" + "/GenerateDotNetHtmlApiRef"
+            },
+            new DotNetHtmlOptions
+            {
+                ProductKey = "API_REF_PRODUCT_KEY",
+                ZipFilePath = "zipPath.ToString()"
+            });
+        }
+    }
+}
